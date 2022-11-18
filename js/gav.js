12b586d634c280f7b26e31d7b0c77d8ae3e647d1
mspolermo <mspolermo@gmail.com>
2022-11-18 14:17:26 +0500

    updated JS

--- js/gav.js
+++ js/gav.js
@@ -4,3 +4,10 @@ if (data) {
 } else {
     alert("Ррр-рррр-рррр!!!!");
 }
+
+var abc = document.getElementsByTagName('span');
+for (var i = 0; i < abc.length; i++) {
+    console.log(abc[i].innerHTML);
+}
+
+document.getElementById('lolkek').style.color = "red";
