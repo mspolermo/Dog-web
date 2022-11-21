f2ba6b2c6fd943ab708f3196f3590ee1e05d3312
mspolermo <mspolermo@gmail.com>
2022-11-21 14:17:51 +0500

    registerform was completed in JS

--- js/gav.js
+++ js/gav.js
@@ -11,3 +11,14 @@ for (var i = 0; i < abc.length; i++) {
 }
 
 document.getElementById('lolkek').style.color = "red";
+
+function registerForm() {
+    var userName = document.getElementById('user_name');
+    var email = document.getElementById('email');
+    var pol = document.getElementById('pol');
+    var user_pass = document.getElementById('user_pass');
+    var user_repass = document.getElementById('user_pass');
+    var res = ("Привет" + userName);
+    console.log(res);
+    return res;
+}
--- js/general.js
+++ js/general.js
@@ -2,3 +2,43 @@
     alert('Функция входа еще не реализована')
     window.location = "register.html";
 }
+
+function registerForm(el) {
+    var userName = el.user_name.value;
+    var email = el.email.value;
+    var pol = el.pol.value;
+    var userPass = el.user_pass.value;
+    var userRepass = el.user_repass.value;
+    var res;
+
+    if (userName == "" || email == "" || pol == "" || userPass == "" || userRepass == "") {
+        res = ("Заполните все поля");
+    }
+    else if (userName.length < 2 || userName.length > 20) {
+        res = ("Неккоректная длина имени");
+    }
+    else if (userPass !== userRepass) {
+        res = ("Введенные пароли не совпадают")
+    }
+    else if (userPass.length < 6) {
+        res = ("Пароль должен состоять не менее чем из 6 символов");
+    }
+
+    if (res !== undefined) {
+        document.getElementById('error').innerHTML = res;
+        return false;
+    }
+    else {
+        if (pol == "Мужской") {
+            alert("Привет, " + userName + "! Теперь ты мой друг! Я отправлю тебе подтверждение нашей дружбы на твою почту - "
+                + email + ". Хорошего дня!");
+            return true;
+        }
+        else {
+            alert("Привет, " + userName + "! Теперь ты моя подруга! Я отправлю тебе подтверждение нашей дружбы на твою почту - "
+                + email + ". Хорошего дня!");
+            return true;
+        }
+    }
+
+}
