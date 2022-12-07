bf5de883d370d5fb719ca1a321995bbf7163e40e
mspolermo <mspolermo@gmail.com>
2022-12-07 16:05:59 +0500

    video player created without design

--- js/gav.js
+++ js/gav.js
@@ -4,12 +4,16 @@ document.querySelector('#stop').onclick = stop;
 document.querySelector('#speed-up').onclick = speedUp;
 document.querySelector('#speed-down').onclick = speedDown;
 document.querySelector('#speed-normal').onclick = speedNormal;
-document.querySelector('#volume').onclick = videoVolume;
+document.querySelector('#volume').oninput = videoVolume;
 
 let video;
 let display;
+let progress;
 
 video = document.querySelector('#video-player');
+progress = document.querySelector('#progress');
+video.ontimeupdate = progressUpdate;
+progress.onclick = videoRewind;
 
 function play() {
     video.play();
@@ -21,7 +25,36 @@ function stop() {
     video.pause();
     video.currentTime = 0;
 };
-function speedUp() {};
-function speedDown() {};
-function speedNormal() {};
-function videoVolume() {};
+function speedUp() {
+    video.play()
+    video.playbackRate = 2;
+};
+function speedDown() {
+    video.play()
+    video.playbackRate = 0.5;
+};
+function speedNormal() {
+    video.play()
+    video.playbackRate = 1;
+};
+function videoVolume() {
+    let v = this.value;
+    video.volume = v /100;
+};
+
+function progressUpdate () {
+    let d = video.duration;
+    let c = video.currentTime;
+    progress.value = (100 * c) / d;
+    vremya = "00:"+Math.round(video.currentTime)+" сек";
+    document.querySelector('#out').innerHTML = vremya;
+};
+
+function videoRewind () {
+let w = this.offsetWidth; /*Определяем ширину саундпрогресса*/
+let o = event.offsetX; /*Определяем значение куда щелкнули на саундпрогрессе*/
+this.value = 100 * o / w;
+video.pause();
+video.currentTime = video.duration * (o / w);
+video.play();
+};
